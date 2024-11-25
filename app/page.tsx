42f35e01ca490db59d812d213c44a36ff1c0ec2f
FedyBenMahfoudh <fedy.ben.mahfoudh@gmail.com>
2024-11-25 18:02:05 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className= "text-white text-3xl underline">
+        this is the home 123333333
+      </h1>
+      <Button>click me</Button>
+    </div>
+  );
+}
